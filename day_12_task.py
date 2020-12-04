a0cd523c809512fe8d9141787e1d9cf9d7af9db9
Rahul Chandan Myakal <74832006+rahulmyakal@users.noreply.github.com>
2020-12-04 23:44:44 +0530

    Delete day_12_task.py

--- day_12_task.py
+++
@@ -1,9 +0,0 @@
-import json
-x = '{"name": "Sachin","age": 18,"city": "Mumbai"}'
-print(json.loads(x)) 
-
-values = [1,2,3,4,5]
-print(json.dumps(values))
-
-values = {1:"A", 2:"B", 3:"C"}
-print(json.dumps(values, indent=4))
